b73601258f5b666bed7a8940b6fdb151be87fdfe
neptune-glitch <Pathakbharti661@gmail.com>
2026-07-07 00:05:14 +0530

    Automatically build ChromaDB from CSV

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ div.stButton>button{
 # ---------------------------------------------------
 with st.sidebar:
 
-    st.image(st.image("images/logo.png", width=70)
+    st.image(st.image("images/logo.png", width=70))
 
     st.title("EcoCycle AI")
 
